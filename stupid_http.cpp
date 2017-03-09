8e0229f099148c74b3fe477586d681785f1cba7b
Luca Versari <veluca93@gmail.com>
2017-03-10 00:20:45 +0100

    Add a simple http server

---
+++ stupid_http.cpp
@@ -0,0 +1,10 @@
+#include "coronet.hpp"
+
+int main() {
+    coronet coro;
+    coro.listen("0.0.0.0", 8080, [&](int fd) {
+        const char resp[] = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-type: text/plain\r\nContent-length: 10\r\n\r\nIt works!\n";
+        coro.write(fd, (const uint8_t*) resp, sizeof(resp)-1);
+    });
+    coro.run();
+}
